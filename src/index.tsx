ce39a3987221fbc7c0414c8e13604ca2118d6061
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-06-18 12:32:55 -0400

    add dall-e

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import { ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
+import theme from "./theme";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
-import theme from "./theme";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
